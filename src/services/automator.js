f10f503d5687a6cd11a8e63d1c0a34a5e5610dc1
Bob Yuwono <bob_yuwono@apps.ipb.ac.id>
2021-12-27 23:57:01 +0700

    add automator function
    adding function to generate ansible playbook command

--- src/services/automator.js
+++ src/services/automator.js
@@ -1,5 +1,24 @@
 // TODO: Implement Ansible logic
 // TODO: Implement Notifier
+const {exec} = require('child_process');
+const { stdout, stderr } = require('process');
 
-export {}
+module.exports = {
+    generatePlaybookLink
+}
 
+
+
+function generatePlaybookLink(address,service){
+    const templateString = `ansible-playbook -e device=${address} -e service=${service} ../playbooks/restart-service.yml`
+    exec(templateString, (error, stdout, stderr) => {
+        if(error){
+            console.error(`error: ${error.message}`);
+        }
+        if (stderr){
+            console.error(`stderr: ${stderr}`);
+        }
+        console.log(`stdout:\n${stdout}`);
+    })
+    return templateString
+}   
