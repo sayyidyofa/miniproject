c40c231a9c5f7b42d6f03538cddb3ba6fcf6f602
basthan <basthanihsan@student.upi.edu>
2021-12-27 14:18:31 +0700

    Add .env package

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 
 import express from 'express';
 import listener from './services/listener.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
