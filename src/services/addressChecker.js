f1a84b20f682bf25d84a577b2f53758ed75fa07c
Bob Yuwono <bob_yuwono@apps.ipb.ac.id>
2021-12-28 00:43:26 +0700

    add addressChecker
    add funtion to check wether the incoming request 'address' detail has been registered in the system

---
+++ src/services/addressChecker.js
@@ -0,0 +1,20 @@
+module.exports = {
+    checkAddress
+}
+
+
+// dummy list ip
+const daftarIP = ['127','128','129']
+
+
+function checkAddress(address){
+
+    const stringifiedAddress = String(address)
+    if (daftarIP.includes(stringifiedAddress)){
+        console.log('ada, ip nya adalah', stringifiedAddress)
+        return true
+    } else{
+        console.log('gaada, ip nya adalah', stringifiedAddress)
+        return false
+    }
+} 
